8a82a7ca40850ea5ea31269b179a6386ded2ba54
Phillip Smith <phillipsmith@Mac.attlocal.net>
2025-06-12 09:16:21 -0500

    initial spotify files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+load_dotenv()
+
+client_id= os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+print(client_id, client_secret)
 
 @app.get("/")
 async def root():
